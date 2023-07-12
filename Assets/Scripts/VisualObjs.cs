8ba29cade85d1726810c67f2c317874e33ecdf6b
jing <shajingyuan@me.com>
2023-07-12 16:04:22 +0200

    update dataset

--- Assets/Scripts/VisualObjs.cs
+++ Assets/Scripts/VisualObjs.cs
@@ -57,7 +57,7 @@ public class VisualObjs : MonoBehaviour
     public float tableScale;
     public float objScale;
     public float posScale;
-    
+
     private GameObject tableInst;
     private string[] _sceneType = { "train", "test", "val", "EOF" };
     private int _sceneTypeCounter = 0;
@@ -372,9 +372,9 @@ public class VisualObjs : MonoBehaviour
         {
             int objIdx = 0;
             Vector3 randomCenter = new Vector3(
-                table.transform.position[0] + UnityEngine.Random.Range(-(_tableWidth / 5), (_tableWidth / 5)),
+                table.transform.position[0] /*+UnityEngine.Random.Range(-(_tableWidth / 5), (_tableWidth / 5)) */,
                 table.transform.position[1] + _tableHeight * 0.5F,
-                table.transform.position[2] + UnityEngine.Random.Range(-(_tableLength / 5), (_tableLength / 5)));
+                table.transform.position[2] /* + UnityEngine.Random.Range(-(_tableLength / 5), (_tableLength / 5)) */);
 
             // add rule objects
 
@@ -440,7 +440,7 @@ public class VisualObjs : MonoBehaviour
     ObjectStruct GetRulePos(int objId, RuleJson.ObjProp objProp, ObjectStruct obj, Vector3 center)
     {
         int num_tries = 0;
-        
+
         float obj_radius = objScale * UnifyRadius;
         Vector3 obj_pos;
         // find a 3D position for the new object
